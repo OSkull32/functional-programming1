f867ab229cf3d714c68d7d8f465f311804001317
Владимир Данченко <368087@niuitmo.ru>
2024-10-08 12:24:02 +0300

    добавил отчет к 1 лабораторной

--- src/lab1/task23/lazy.clj
+++ src/lab1/task23/lazy.clj
@@ -1,25 +1,31 @@
 (ns lab1.task23.lazy)
 
+; Возвращает список всех делителей числа n
 (defn proper-divisors [n]
   (filter (fn [d] (zero? (mod n d)))
           (range 1 (inc (/ n 2)))))
 
+;  Проверяет, является ли число n избыточным.
 (defn abundant? [n]
   (> (reduce + (proper-divisors n)) n))
 
+; Генерирует бесконечную последовательность избыточных чисел.
 (defn abundant-numbers []
   (filter abundant? (iterate inc 1)))
 
 (def limit 28123)
 
+; Создает вектор всех избыточных чисел, меньших 28124.
 (def abundant-nums (take-while #(< % 28124) (abundant-numbers)))
 
+; Вычисляет все возможные суммы двух избыточных чисел.
 (defn sums-of-two-abundant []
   (let [abundant (vec abundant-nums)]
     (for [a abundant
           b abundant]
       (+ a b))))
 
+; Находит все числа, которые не могут быть представлены как сумма двух избыточных чисел.
 (defn cannot-be-written-as-sum []
   (let [abundant-sums (into #{} (sums-of-two-abundant))
         all-nums (range 1 limit)]
